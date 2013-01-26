7d99ceb4255161e211e6c172c84c121b9d24afa8
Zachary Johnson <me@zachstronaut.com>
2013-01-25 21:35:51 -0600

    very basic socket.io

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require('express'),
     config = require('./config/config-app'),
-    app = express.createServer();
+    app = express.createServer(),
+    io = require('socket.io').listen(app);
 
 app.use(express.static(config.public));
 
 app.listen(3000);
+
+// Socket.io code uncoupled from HTTP server!!
+io.sockets.on('connection', function (socket) {
+    socket.emit('data', {hello: 'world'});
+});
