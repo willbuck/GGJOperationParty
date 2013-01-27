c36e9cddd154fe5f157029a543efb2c06595d636
Zachary Johnson <me@zachstronaut.com>
2013-01-27 01:15:48 -0600

    players can join real named lobbies bit of a css upgrade to player names in lobbies don't actually leave lobby screen until all players are ready show waiting state once you have marked that you are ready

--- public/js/internal/gameStates.js
+++ public/js/internal/gameStates.js
@@ -1,32 +1,40 @@
 var socket = io.connect(window.location.hostname);
 var GameStates = (function(socket) {
-    var joinGame = function(lobbyName) {
+    var myLobby,
+        joinGame = function(lobbyName) {
         // Join a named lobby
         console.log('Joining game: ' + lobbyName);
         socket.emit('join', {lobby: lobbyName});
+        myLobby = lobbyName;
     };
     return {
-        loadStartScreen: function() {
-            TemplateUtil.loadGameScreen('selectGameScreen', {lobbies: [{gameID: 1, name: 'test'}]});
+        loadStartScreen: function (lobbies) {
+            TemplateUtil.loadGameScreen('selectGameScreen', {lobbies: lobbies});
             $('.modalDialog').addClass('active');
             $('.gameSelect').on('touch click', function(e) {
                 console.log('Selecting game: ' + e.toElement.value);
                 joinGame(e.toElement.value);
             });
         },
-        loadLobbyScreen: function(players) {
+        loadLobbyScreen: function (players) {
             TemplateUtil.loadGameScreen('lobbyScreen', {
-                lobby: {name: 'test'},
+                lobby: {name: myLobby},
                 players: players
             });
+            
             $('.startGame').on('touch click', function(e) {
-                console.log('Time to Start!');
                 // When you are ready, tell the lobby (when everybody in the lobby is ready the game starts)
                 socket.emit('ready');
-                $('.modalDialog').removeClass('active');
-                TemplateUtil.loadWidget(setupButtonPushOn);
+                
+                $('.startGame').hide();
+                $('#waitingMsg').show();
+                
+                //TemplateUtil.loadWidget(setupButtonPushOn);
                 HeartBeatAudio.play();
             });
+        },
+        playGame: function () {
+            $('.modalDialog').removeClass('active');
         }
     }
 })(socket);
--- public/js/internal/templates/templates.js
+++ public/js/internal/templates/templates.js
@@ -28,21 +28,21 @@ templates['lobbyScreen'] = template(function (Handlebars,depth0,helpers,partials
 function program1(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += "\n        <span class=\"waiting\">";
+  buffer += "\n        <li class=\"waiting\">";
   stack1 = typeof depth0 === functionType ? depth0.apply(depth0) : depth0;
-  buffer += escapeExpression(stack1) + "</span>\n    ";
+  buffer += escapeExpression(stack1) + "</li>\n    ";
   return buffer;}
 
   buffer += "<h3>Welcome to Game: ";
   stack1 = depth0.lobby;
   stack1 = stack1 == null || stack1 === false ? stack1 : stack1.name;
   stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1;
-  buffer += escapeExpression(stack1) + "</h3>\n<p>Players currently in lobby:</p>\n<ul>\n    ";
+  buffer += escapeExpression(stack1) + "</h3>\n<p>Players currently in lobby:</p>\n<ul class=\"waitingList\">\n    ";
   stack1 = depth0.players;
   stack2 = {};
   stack1 = helpers.each.call(depth0, stack1, {hash:stack2,inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n</ul>\n<input type=\"button\" class=\"startGame\" value=\"I'm Ready!!\">";
+  buffer += "\n</ul>\n<input type=\"button\" class=\"startGame\" value=\"I'm Ready!!\">\n<p id=\"waitingMsg\">Waiting...</p>";
   return buffer;});
 templates['selectGameScreen'] = template(function (Handlebars,depth0,helpers,partials,data) {
   helpers = helpers || Handlebars.helpers; data = data || {};
@@ -51,12 +51,8 @@ templates['selectGameScreen'] = template(function (Handlebars,depth0,helpers,par
 function program1(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += "\n    <li><input class=\"gameSelect\" type=\"button\" id=\"";
-  stack1 = depth0.gameID;
-  stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1;
-  buffer += escapeExpression(stack1) + "\" value=\"";
-  stack1 = depth0.name;
-  stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1;
+  buffer += "\n    <li><input class=\"gameSelect\" type=\"button\" value=\"";
+  stack1 = typeof depth0 === functionType ? depth0.apply(depth0) : depth0;
   buffer += escapeExpression(stack1) + "\"/></li>\n";
   return buffer;}
 
