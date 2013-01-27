8bed9a6e0ac68ff9edf2c1d8eb095a7fa908b6e1
Will Buck <parkerbuck@yahoo.com>
2013-01-26 22:36:45 -0600

    Massaging the audio (and putting it in touch/click handler for mobile)

--- public/js/internal/audio.js
+++ public/js/internal/audio.js
@@ -2,5 +2,6 @@ var HeartBeatAudio = (function() {
     var audio = document.getElementById('heartbeatAudio');
     audio.playbackRate = 0.5;
     audio.play();
+    audio.pause();
     return audio;
 })();
