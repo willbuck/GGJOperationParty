46c12b5b12b81fc598d44dc9b752a1f3d6966ba6
Will Buck <parkerbuck@yahoo.com>
2013-01-26 19:43:23 -0600

    Trying to better modularize, socket initialization needs to happen somewhere else though

--- public/js/internal/gameStates.js
+++ public/js/internal/gameStates.js
@@ -1,4 +1,5 @@
-var GameStates = (function() {
+var socket = io.connect(window.location.hostname);
+var GameStates = (function(socket) {
     var joinGame = function(lobbyName) {
         // Join a named lobby
         console.log('Joining game: ' + lobbyName);
@@ -9,14 +10,23 @@ var GameStates = (function() {
             loadGameScreen('selectGameScreen', {lobbies: [{gameID: 1, name: 'test'}]});
             $('.modalDialog').addClass('active');
             $('.gameSelect').on('touch click', function(e) {
-                console.log(e);
+                console.log('Selecting game: ' + e.toElement.value);
                 joinGame(e.toElement.value);
             });
         },
-        loadLobbyScreen: function() {
-
+        loadLobbyScreen: function(players) {
+            loadGameScreen('lobbyScreen', {
+                lobby: {name: 'test'},
+                players: players
+            });
+            $('.startGame').on('touch click', function(e) {
+                console.log('Time to Start!');
+                // When you are ready, tell the lobby (when everybody in the lobby is ready the game starts)
+                socket.emit('ready');
+                $('.modalDialog').removeClass('active');
+            });
         }
     }
-})();
+})(socket);
 
 
--- public/js/internal/socketHandlers.js
+++ public/js/internal/socketHandlers.js
@@ -1,63 +1,53 @@
-console.log('Loading Socket.io connection');
+var SocketHandlers = (function() {
+    console.log('Loading Socket.io connection');
 
-//var socketEndpointUrl = environment.dev ? 'http://localhost' : 'http://ggj-2013-operation-party.herokuapp.com/';
-var socketEndpointUrl = 'http://localhost';
-var GameData = {
-        joined : false,
-        players : []
-    },
-    socket = io.connect(window.location.hostname);
+    var GameData = {
+            joined : false,
+            players : []
+        }
 
-// Get named lobbies
-socket.on('lobbies', function (data) {
-    if (GameData.joined) {
-        return;
-    }
-    GameStates.loadStartScreen();
-
-    // You joined your lobby successfully
-    socket.on('joined', function () {
+    // Get named lobbies
+    socket.on('lobbies', function (data) {
         if (GameData.joined) {
             return;
         }
+        GameStates.loadStartScreen();
 
-        console.log('Joined');
-        GameData.joined = true;
-        // Every time a new player joins the lobby we get a list of *all* current players
-        socket.on('playerList', function (data) {
-            console.log(data.players);
-            GameData.players = data.players;
-            loadGameScreen('lobbyScreen', {
-                lobby: {name: 'test'},
-                players: GameData.players
-            });
-            $('.startGame').on('touch click', function(e) {
-                console.log('Time to Start!');
-                // When you are ready, tell the lobby (when everybody in the lobby is ready the game starts)
-                socket.emit('ready');
-                $('.modalDialog').removeClass('active');
+        // You joined your lobby successfully
+        socket.on('joined', function () {
+            if (GameData.joined) {
+                return;
+            }
+
+            console.log('Joined');
+            GameData.joined = true;
+            // Every time a new player joins the lobby we get a list of *all* current players
+            socket.on('playerList', function (data) {
+                console.log(data.players);
+                GameData.players = data.players;
+                GameStates.loadLobbyScreen(GameData.players);
             });
         });
-    });
 
-    // Hey the game started!
-    socket.on('play', function () {
-        console.log('play!');
-    });
+        // Hey the game started!
+        socket.on('play', function () {
+            console.log('play!');
+        });
 
-    socket.on('task', function (data) {
-        console.log('Task came with data: ' + data);
-    });
+        socket.on('task', function (data) {
+            console.log('Task came with data: ' + data);
+        });
 
-    socket.on('loadWidgets', function(data) {
-        console.log('Loading and inserting templates: ' + data);
-        _.each(data.widgets, function(widget) {
-            console.log('Inserting ' + widget.name + ' at ' + widget.destination + ' with controlName ' + widget.data.controlName);
-            loadWidget(widget.name, widget.destination, widget.data);
+        socket.on('loadWidgets', function(data) {
+            console.log('Loading and inserting templates: ' + data);
+            _.each(data.widgets, function(widget) {
+                console.log('Inserting ' + widget.name + ' at ' + widget.destination + ' with controlName ' + widget.data.controlName);
+                loadWidget(widget.name, widget.destination, widget.data);
+            });
+        });
+
+        socket.on('disconnect', function () {
+            socket.disconnect();
         });
     });
-
-    socket.on('disconnect', function () {
-        socket.disconnect();
-    });
-});
+})();
